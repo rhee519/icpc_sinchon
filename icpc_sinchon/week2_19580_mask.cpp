de21ea6a84e220abfc291634cba9d3e2f72d6fc2
Jiho Rhee <rhee519@gmail.com>
2021-01-19 20:16:26 +0900

    2021/01/19 20:16

--- icpc_sinchon/week2_19580_mask.cpp
+++ icpc_sinchon/week2_19580_mask.cpp
@@ -2,8 +2,26 @@
 #include <map>
 
 using namespace std;
+#define SIZE        10 + 1
+
+bool myComp(pair<long long, long long> p1, pair<long long, long long> p2) {
+    if(p1.second == p2.second) return p2.first < p1.first;
+    return p1.second < p2.second;
+}
+
+map<long long, int> m;    // <price of mask, # of masks left>
+pair<long long, long long> r[SIZE];     // <lower_bound, upper_bound>
 
 int main( ) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    freopen("input.txt", "r", stdin);
+    
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        cin >> r[i].first >> r[i].second;
+    }
+    
     
     return 0;
 }
