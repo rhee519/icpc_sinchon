bbc11018a12a9358151d8174afe0b60262fee416
Jiho Rhee <rhee519@gmail.com>
2021-01-21 22:41:29 +0900

    2021/01/21 22:41

---
+++ icpc_sinchon/week3_20040_cyclegame.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+#define SIZE        10 + 1
+
+int root[SIZE];
+
+void init(int n) {
+    for(int i = 0; i < n; i++) {
+        root[i] = i;
+    }
+}
+
+
+
+int main( ) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    return 0;
+}
+
