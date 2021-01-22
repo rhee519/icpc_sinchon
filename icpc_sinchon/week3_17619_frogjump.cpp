826d2bc159c7e417f186df9663bcdd98df8fc2e6
Jiho Rhee <rhee519@gmail.com>
2021-01-22 12:55:30 +0900

    2021/01/22 12:55

--- icpc_sinchon/week3_17619_frogjump.cpp
+++ icpc_sinchon/week3_17619_frogjump.cpp
@@ -1,8 +1,44 @@
-//
-//  week3_17619_frogjump.cpp
-//  icpc_sinchon
-//
-//  Created by Jiho Rhee on 2021/01/22.
-//
+#include <iostream>
+#include <algorithm>
 
-#include <stdio.h>
+using namespace std;
+#define SIZE        10 + 1
+
+struct line{
+    int idx;
+    int x1, x2;
+    
+    bool operator<(const line &other) const{
+        if(x1 == other.x1) return (x1 < x2);
+        return x1 < other.x1;
+    }
+};
+
+int parent[SIZE];
+line arr[SIZE];
+
+void connect(int n) {
+    
+}
+
+bool canJump(int n, int idx1, int idx2) {
+    
+    return false;
+}
+
+int main( ) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    freopen("input.txt", "r", stdin);
+    
+    int n, q;
+    cin >> n >> q;
+    for(int i = 1; i <= n; i++) {
+        int y;
+        cin >> arr[i].x1 >> arr[i].x2 >> y;
+        arr[i].idx = i;
+    }
+    
+    
+    
+    return 0;
+}
