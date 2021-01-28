75403b7da5edf38d19f18ec90a79b10d638deff7
Jiho Rhee <rhee519@gmail.com>
2021-01-28 18:31:33 +0900

    clear!

--- icpc_sinchon/week5_1849_permutation.cpp
+++ icpc_sinchon/week5_1849_permutation.cpp
@@ -2,24 +2,55 @@
 #include <algorithm>
 
 using namespace std;
+#define MAX_SIZE        1'000'000 + 1
+int seg[MAX_SIZE * 4];
+int p[MAX_SIZE];
 
-const int ST = 1<<17;
-
-int seg[ST*2];
-
-void build( ) {
-    for(int i = ST-1; i >= 1; i--) {
-        seg[i] = seg[i*2] + seg[i*2+1];
-    }
+int update(int pos, int val, int node, int left, int right) {
+    // update arr[pos] to val
+    // && return seg[node] == sum of arr[left] ~ arr[right]
+    
+    if(pos < left || right < pos)
+        return seg[node];
+    
+    // left <= pos <= right
+    if(left == right)
+        return seg[node] = val;
+    int mid = (left + right)/2;
+    return seg[node] = update(pos, val, node*2, left, mid) + update(pos, val, node*2+1, mid+1, right);
 }
 
-void update(int idx, int val) {
-    int i = ST + idx;
+int query(int val, int node, int left, int right) {
+    // return pos of val
+    if(left == right) return left;
+    
+    int mid = (left + right)/2;
+    if(seg[node*2] >= val) // left <= pos <= mid
+        return query(val, node*2, left, mid);
+    // mid+1 <= pos <= right
+    return query(val-seg[node*2], node*2+1, mid+1, right);
 }
 
 int main( ) {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
     freopen("input.txt", "r", stdin);
+    int n; cin >> n;
+    for(int i = 1; i <= n; i++) {
+        update(i, 1, 1, 1, n);
+    }
+    
+    for(int i = 1; i <= n; i++) {
+        int pos; cin >> pos;
+        int q = query(pos+1, 1, 1, n);
+        p[q] = i;
+        update(q, 0, 1, 1, n);
+    }
+    
+    // print
+    for(int i = 1; i <= n; i++) {
+        cout << p[i] << '\n';
+    }
     
     return 0;
 }
+
